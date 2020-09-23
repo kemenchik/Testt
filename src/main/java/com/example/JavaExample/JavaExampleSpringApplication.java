fe47a572db905f2e0fae2e96befdee7ee6947050
kemenchik <skl@internativa.biz>
2020-09-23 18:56:12 +0300

    Start

---
+++ src/main/java/com/example/JavaExample/JavaExampleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExampleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExampleSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaExample/JavaExampleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExampleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
